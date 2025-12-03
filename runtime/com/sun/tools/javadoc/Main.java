29a79b2c103fd4584c29033ae990c3ad2964f32c
Pavel Shirshov <pshirshov@eml.cc>
2025-12-03 21:27:05 +0000

    wip

--- runtime/com/sun/tools/javadoc/Main.java
+++ runtime/com/sun/tools/javadoc/Main.java
@@ -3,6 +3,9 @@ package com.sun.tools.javadoc;
 import java.io.PrintWriter;
 
 public class Main {
+    public static int execute(String programName, PrintWriter errWriter, PrintWriter warnWriter, PrintWriter noticeWriter, String defaultDocletClassName, String[] args) {
+        return 0;
+    }
     public static int execute(String programName, PrintWriter errWriter, PrintWriter warnWriter, PrintWriter noticeWriter, String defaultDocletClassName, ClassLoader classLoader, String[] args) {
         return 0;
     }
